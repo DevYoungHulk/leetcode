6dbda9ac5bce000607a59a60deba61f245e37da1
hulk.yang <hulk.yang@homecredit.cn>
2021-01-26 17:39:06 +0800

    [TREE] add

---
+++ src/test/java/tree/N114_FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,36 @@
+package tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+import tree.model.TreeNode;
+
+public class N114_FlattenBinaryTreeToLinkedList {
+    @Test
+    public void N114_FlattenBinaryTreeToLinkedList() {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(5, null, new TreeNode(6)));
+        flatten(root);
+        Assert.assertEquals(root.val, 1);
+        root = root.right;
+        Assert.assertEquals(root.val, 2);
+        root = root.right;
+        Assert.assertEquals(root.val, 3);
+        root = root.right;
+        Assert.assertEquals(root.val, 4);
+        root = root.right;
+        Assert.assertEquals(root.val, 5);
+        root = root.right;
+        Assert.assertEquals(root.val, 6);
+    }
+
+    TreeNode tmp = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.left = null;
+        root.right = tmp;
+        tmp = root;
+    }
+}
